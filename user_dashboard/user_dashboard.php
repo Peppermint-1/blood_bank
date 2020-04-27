4269b98a3d9e23f5c49fbef68a6dd4427cb93aa8
Peppermint-1 <peppermint.assn@gmail.com>
2020-04-27 11:16:02 +0530

    First

---
+++ user_dashboard/user_dashboard.php
@@ -0,0 +1,76 @@
+<?php
+	include('user_header.php');
+?>
+
+		<div class="main">
+			<!-- MAIN CONTENT -->
+			<div class="main-content">
+				<div class="container-fluid">
+					<!-- OVERVIEW -->
+					<div class="panel panel-headline">
+						<div class="panel-heading">
+							<h3 class="panel-title">Weekly Overview</h3>
+							<p class="panel-subtitle">Period: Jan 1, 2020 - Dec 31, 2020</p>
+						</div>
+						<div class="panel-body">
+							<div class="row">
+								<div class="col-md-3">
+									<div class="metric">
+										<span class="icon"><i class="fa fa-download"></i></span>
+										<p>
+											<span class="number">0</span>
+											<span class="title">Entries in Last 7 Days</span>
+										</p>
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="metric">
+										<span class="icon"><i class="fa fa-shopping-bag"></i></span>
+										<p>
+											<span class="number">0</span>
+											<span class="title">Supply</span>
+										</p>
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="metric">
+										<span class="icon"><i class="fa fa-eye"></i></span>
+										<p>
+											<span class="number">5</span>
+											<span class="title">Visits</span>
+										</p>
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="metric">
+										<span class="icon"><i class="fa fa-bar-chart"></i></span>
+										<p>
+											<span class="number">0%</span>
+											<span class="title">Conversions</span>
+										</p>
+									</div>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+					<!-- END OVERVIEW -->
+					
+				
+					
+				</div>
+			</div>
+			<!-- END MAIN CONTENT -->
+		</div>
+		<!-- END MAIN -->
+		<div class="clearfix"></div>
+		<footer>
+			<div class="container-fluid">
+				<p class="copyright">&copy; 2020 Brought To You By Raisoni Group</a>.</p>
+			</div>
+		</footer>
+	</div>
+
+<?php
+	include('user_footer.php');
+?>
